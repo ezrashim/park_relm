833463967e4a37416dd0944ea9e1aa999eb3b4ce
Leon <Leon@Leons-MacBook-Pro.local>
2016-01-06 14:58:11 -0500

    added show page for parks with test

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,6 +1,14 @@
 class ParksController < ApplicationController
 
   def index
+
     @parks = Park.all
+    # binding.pry
+  end
+
+  def show
+
+    @park = Park.find(params[:id])
+    # binding.pry
   end
 end
---
+++ spec/features/parks/02_parks_show_spec.rb
@@ -0,0 +1,49 @@
+require "rails_helper"
+#
+# As an authenticated user
+# I want to view a list of items
+# So that I can pick items to review
+
+
+feature "authenticated users can view parks" do
+  scenario "visitor signs in and views parks on index page" do
+    parks = FactoryGirl.create_list(:park, 3)
+
+    visit root_path
+
+    expect(page).to have_content "Parks"
+
+    parks.each do |park|
+      visit park_path(park)
+      expect(page).to have_content(park.title)
+      expect(page).to have_content(park.location)
+      expect(page).to have_content(park.rating)
+    end
+  end
+
+
+
+  let(:park){ Park.create!(title: "Commons", location: "Boston", rating: 10, bathroom: true, picnic: true, pets: true, basketball: true, baseball: true)}
+
+
+  scenario "visitor signs in and views parks on index page" do
+
+
+    visit root_path
+
+    expect(page).to have_content "Parks"
+
+# binding.pry
+    visit park_path(park)
+    # binding.pry
+    expect(page).to have_content(park.title)
+    expect(page).to have_content(park.location)
+    expect(page).to have_content(park.rating)
+    expect(page).to have_content("Bathrooms available")
+    expect(page).to have_content("Picnic facilities available")
+    expect(page).to have_content("Pets allowed")
+    expect(page).to have_content("basketball courts available")
+    expect(page).to have_content("Baseball field available")
+
+  end
+end
