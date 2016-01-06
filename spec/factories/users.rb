754c5af5dcdb4fd5d83d1b555720fe69e7e96fb2
Ezra Shim <ezrashim@gmail.com>
2016-01-06 09:24:44 -0500

    authentication udpate

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,10 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence(:email) { |n| "user#{n}@email.com" }
+    sequence(:first_name) { |n| "Relm#{n}" }
+    sequence(:last_name) { |n| "Park#{n}" }
+    admin false
+    avatar_url "http://2.bp.blogspot.com/-HzFJhEY3KtU/Tea7Ku92cpI/AAAAAAAAALw/uBMzwdFi_kA/s400/1.jpg"
   end
 
 end
