c0c04ab5cc4751ec9513978dc9043ff7706b7500
Roman Levitas <levitas1@illinois.edu>
2016-01-08 18:38:34 -0500

    removed uncessesary lines from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   root 'parks#index'
-  # devise_for :users
+
   devise_for :users, controllers: { registrations: 'registrations' }
   resources :users, only: [:show]
 
   resources :parks do
     resources :reviews, only: [:create]
   end
-
-  # resources :reviews, only: [:create]
 end
