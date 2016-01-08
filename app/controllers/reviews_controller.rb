69c86ef384799d08a7f8443b4ee7f47bfe46c496
Roman Levitas <levitas1@illinois.edu>
2016-01-08 18:41:26 -0500

    addressed hound

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,4 @@
 class ReviewsController < ApplicationController
-
-
   def create
     @park = Park.find(params[:park_id])
     @review = Review.new(review_params)
@@ -17,9 +15,9 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    r_params = params.require(:review).permit(:title, :body,)
+    r_params = params.require(:review).permit(:title, :body)
     r_params.store(:park_id, params[:park_id])
-    r_params.store( :user_id ,current_user.id)
+    r_params.store( :user_id, current_user.id)
     return r_params
   end
 end
