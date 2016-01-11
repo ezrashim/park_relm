438ef87875df670c77cab3c19f0897d1dd051b93
Ezra Shim <ezrashim@gmail.com>
2016-01-11 16:35:38 -0500

    refactored root_path redundancy spec

--- spec/features/parks/03_parks_create_spec.rb
+++ spec/features/parks/03_parks_create_spec.rb
@@ -7,11 +7,7 @@ require "rails_helper"
 feature "authenticated users can view parks" do
   let!(:user) { create(:user) }
   scenario "visitor signs in and views parks on index page" do
-
-    visit root_path
-
     sign_in_as(user)
-
     click_link("New Park")
     fill_in("Title", with: "Central Park")
     fill_in("Location", with: "NYC")
