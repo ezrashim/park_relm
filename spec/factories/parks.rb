ce51cc4e6f9b4eb2c6808d817312859028a1a643
Meder <m.toktosunov@icloud.com>
2016-01-11 13:46:43 -0500

    infinite loop issue with association fixed

--- spec/factories/parks.rb
+++ spec/factories/parks.rb
@@ -1,8 +1,13 @@
 FactoryGirl.define do
   factory :park do
-    # association :review
     sequence(:title) { |n| "park no. #{n}" }
     sequence(:location) { |n| "Location#{n}" }
     rating 10
+
+    factory :park_with_reviews do
+      after(:create) do |new_park|
+        FactoryGirl.create(:review, park: new_park)
+      end
+    end
   end
 end
