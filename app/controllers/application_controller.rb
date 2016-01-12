ac168a420cac2bc84fe158de7fcab29b1987ad06
Leon <Leon@Leons-MacBook-Pro.local>
2016-01-12 11:54:12 -0500

    Hound Comments x2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << [
-      :first_name, 
+      :first_name,
       :last_name,
       :admin,
       :profile_photo
