a1c54ea4d8da6d05cd65e070b3c96a19c9fc7b06
Leon <Leon@Leons-MacBook-Pro.local>
2016-01-12 13:01:35 -0500

    pagination implemented with tests

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -2,7 +2,7 @@ class ParksController < ApplicationController
   before_action :authorize_user, only: [:destroy]
 
   def index
-    @parks = Park.all
+    @parks = Park.order(rating: :desc).page params[:page]
   end
 
   def show
---
+++ spec/features/pagination/parks_pagination_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+#
+# I want to view a list of 10+ parks
+# So that I can see only 10 items per page
+
+feature "users can view parks" do
+  let! (:parks) {create_list(:park_with_reviews, 25)}
+  scenario "visitor signs in and views parks on index page" do
+    visit root_path
+    save_and_open_page
+
+    park_list = parks.sort_by { |park| park.rating }
+    expect(page).to have_content park_list.last.title
+    expect(page).to_not have_content park_list.first.title
+  end
+end
