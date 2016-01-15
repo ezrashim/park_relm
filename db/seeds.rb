9c7a503a7da49d60974a5644eecc4c00eb9b9639
Ezra Shim <ezrashim@gmail.com>
2016-01-15 13:38:39 -0500

    hound comment addressed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-
 AdminSeeder.seed!
 
 if Rails.env.development? || Rails.env.production?
